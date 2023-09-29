50294efc3a6bbf33e1fae7c47f48ccca597d5cc7
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-09-29 14:54:46 +0200

    Add student form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import {Student, studentData} from "./student.ts";
 import StudentGallery from "./StudentGallery.tsx";
 
@@ -7,14 +7,47 @@ function App() {
 
     const [students, setStudents] = useState<Student[]>(studentData)
 
-  return (
-    <>
-        <h1>
-            Student Gallery
-        </h1>
-        <StudentGallery students={students} />
-    </>
-  )
+    const [firstName, setFirstName] = useState<string>("")
+    const [lastName, setLastName] = useState<string>("")
+
+    function onFirstNameChange(event: ChangeEvent<HTMLInputElement>) {
+        setFirstName(event.target.value)
+    }
+
+    function onLastNameChange(event: ChangeEvent<HTMLInputElement>) {
+        setLastName(event.target.value)
+    }
+
+    function onStudentSave(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        const newStudent: Student = {
+            firstName: firstName,
+            lastName: lastName
+        }
+
+        const newStudents = [...students, newStudent]
+        setStudents(newStudents)
+
+        setFirstName("")
+    }
+
+    return (
+        <>
+            <h1>
+                Student Gallery
+            </h1>
+            <StudentGallery students={students}/>
+
+            <form onSubmit={onStudentSave}>
+                <input value={firstName} onChange={onFirstNameChange}/>
+                <input value={lastName} onChange={onLastNameChange}/>
+                <button type="submit">Save</button>
+            </form>
+
+            <button type="button">Other</button>
+        </>
+    )
 }
 
 export default App
